a6b866a56e8203c7ef4ae632e654db29eebbfa17
Zhuo Lu <sethlu@mintkit.net>
2017-12-07 00:24:43 -0800

    Fix code style

--- src/renderers/opengl/OpenGLRenderList.cpp
+++ src/renderers/opengl/OpenGLRenderList.cpp
@@ -4,14 +4,14 @@
 
 namespace renderbox {
 
-    void OpenGLRenderList::addObject(GLuint programID, Object *object) {
-        auto result = objects.find(programID);
+    void OpenGLRenderList::addObject(GLuint programId, Object *object) {
+        auto result = objects.find(programId);
         if (result != objects.end()) {
             result->second.insert(result->second.end(), object);
         } else {
             std::vector<Object *> objectList;
             objectList.insert(objectList.end(), object);
-            objects.insert(std::pair<GLuint, std::vector<Object *>>(programID, objectList));
+            objects.insert(std::pair<GLuint, std::vector<Object *>>(programId, objectList));
         }
     }
 
