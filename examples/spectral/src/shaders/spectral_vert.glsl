90545397844d4ea17fd7b079eac7b4e91621fbb7
Zhuo Lu <sethlu@mintkit.net>
2018-05-07 21:23:15 -0700

    Fix spectral example

--- examples/spectral/src/shaders/spectral_vert.glsl
+++ examples/spectral/src/shaders/spectral_vert.glsl
@@ -3,7 +3,7 @@
 
 uniform mat4 rb_worldProjectionMatrix;
 uniform mat4 rb_worldMatrix;
-uniform mat4 rb_worldNormalMatrix;
+uniform mat3 rb_worldNormalMatrix;
 
 out gl_PerVertex {
     vec4 gl_Position;
@@ -17,7 +17,7 @@ in vec3 rb_vertexNormal;
 void main() {
 
     vertexWorldPosition = vec3(rb_worldMatrix * vec4(rb_vertexPosition, 1));
-    vertexWorldNormal = vec3(rb_worldNormalMatrix * vec4(rb_vertexNormal, 0));
+    vertexWorldNormal = rb_worldNormalMatrix * rb_vertexNormal;
 
     gl_Position = rb_worldProjectionMatrix * vec4(rb_vertexPosition, 1);
 
